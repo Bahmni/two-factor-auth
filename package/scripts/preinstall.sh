099c97b4c3b4d6a6c9dffb6893ca93c286d4e8dc
shashig <shashig@thoughtworks.com>
2016-07-04 11:29:32 +0530

    Shashi, Lavanya | #1942 | Stopping openmrs service before installing bahmni-two-factor-auth

--- package/scripts/preinstall.sh
+++ package/scripts/preinstall.sh
@@ -4,3 +4,5 @@ rm -rf /opt/bahmni-two-factor-auth
 rm -rf /etc/bahmni-two-factor-auth
 rm -rf /etc/init.d/bahmni-two-factor-auth
 rm -rf /var/run/bahmni-two-factor-auth
+
+service openmrs stop || true    
